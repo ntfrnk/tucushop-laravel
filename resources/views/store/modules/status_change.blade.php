a7947dede44d9199b1219296c68cccf394c4d7a6
Franco D. Ocaranza <netfrank777@gmail.com>
2020-05-07 15:17:14 -0300

    Se modificó texto en «activar negocio»

--- resources/views/store/modules/status_change.blade.php
+++ resources/views/store/modules/status_change.blade.php
@@ -12,7 +12,7 @@
 				@if($store->status == 1)
 
 					<div class="f17">
-						<h1 class="f30 marB15">Desactivar negocio</h1>
+						<h1 class="f30 marB15">Deshabilitar negocio</h1>
 						<p>Estás a punto de desactivar tu negocio, y con ello también tus productos y servicios dejarán de estar publicados.</p>
 						<p class="f17 bold marB20">¿Realmente deseas realizar este cambio?</p>
 						<p>
@@ -24,11 +24,11 @@
 				@else
 
 					<div class="f17">
-						<h1 class="f30 marB15">Reactivar negocio</h1>
-						<p>Al realizar esta acción volverás a activar tu negocio. Esta operación también volverá a activar los productos y servicios que tienes publicados.</p>
-						<p class="f17 bold marB20">¿Confirmas que deseas reactivar tu negocio?</p>
+						<h1 class="f30 marB15">Habilitar negocio</h1>
+						<p>Al realizar esta acción se activará tu negocio, y los items que tienes cargados (y habilitados) comenzarán a mostrarse en los listados de la web.</p>
+						<p class="f17 bold marB20">¿Confirmas que deseas realizar esta operación?</p>
 						<p>
-							<a class="btn padLR40 btn-primary marR10" href="{{ route('store.status.change', ['alias' => $store->alias]) }}">Sí, reactivar negocio</a>
+							<a class="btn padLR40 btn-primary marR10" href="{{ route('store.status.change', ['alias' => $store->alias]) }}">Sí, habilitar mi negocio</a>
 							<a class="btn padLR40 btn-outline-secondary" href="{{ route('store.home', ['alias' => $store->alias]) }}">No, volver al inicio</a>
 						</p>
 					</div>
