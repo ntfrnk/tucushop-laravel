42839d149799819c5932e2c57d3433aca21f0320
Franco D. Ocaranza <netfrank777@gmail.com>
2020-05-21 10:10:51 -0300

    Se modifica nombre de variable -item- para evitar conflicto en item detail

--- resources/views/item/includes/item_random.blade.php
+++ resources/views/item/includes/item_random.blade.php
@@ -1,17 +1,17 @@
-<div class="box-item{{ $item->offer ? ' item-offer' : '' }} item-random item">
+<div class="box-item{{ $item_rand->offer ? ' item-offer' : '' }} item-random item">
 	<div class="box-item-body">
 		<figure class="box-item-image">
-			<a href="{{ route('item.detail', ['name' => \UrlFormat::url_limpia($item->name), 'id' => \UrlFormat::add_zeros($item->id)]) }}">
-				<img src="{{ asset('storage/items/sm/'.$item->photos->first()->file_path.'?v='.$item->photos->first()->version) }}" class="img-fluid" />
+			<a href="{{ route('item.detail', ['name' => \UrlFormat::url_limpia($item_rand->name), 'id' => \UrlFormat::add_zeros($item_rand->id)]) }}">
+				<img src="{{ asset('storage/items/sm/'.$item_rand->photos->first()->file_path.'?v='.$item_rand->photos->first()->version) }}" class="img-fluid" />
 			</a>
 		</figure>
 		<div class="box-item-details">
-			<h3 class="ellipsis"><a href="{{ route('item.detail', ['name' => \UrlFormat::url_limpia($item->name), 'id' => \UrlFormat::add_zeros($item->id)]) }}">{{ $item->name }}</a></h3>
-			@if($item->offer)
-				<p><span class="offer">$ {{ $item->price }}</span><span>/</span>$ {{ $item->offer->price }}</p>
-				<div class="box-item-offer-flag">{{ $item->offer->percent }}% OFF</div>
+			<h3 class="ellipsis"><a href="{{ route('item.detail', ['name' => \UrlFormat::url_limpia($item_rand->name), 'id' => \UrlFormat::add_zeros($item_rand->id)]) }}">{{ $item_rand->name }}</a></h3>
+			@if($item_rand->offer)
+				<p><span class="offer">$ {{ $item_rand->price }}</span><span>/</span>$ {{ $item_rand->offer->price }}</p>
+				<div class="box-item-offer-flag">{{ $item_rand->offer->percent }}% OFF</div>
 			@else 
-				<p>$ {{ $item->price }}</p>
+				<p>$ {{ $item_rand->price }}</p>
 			@endif
 		</div>
 	</div>
