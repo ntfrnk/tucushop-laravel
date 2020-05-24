c11d3beca4306fe76e162196cdca6a3c28cc9bfc
Franco D. Ocaranza <netfrank777@gmail.com>
2020-05-23 22:55:15 -0300

    Corrección registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">&nbsp;</label>
+                            <label class="col-md-4 col-form-label text-md-right">&nbsp;</label>
 
                             <div class="col-md-6">
                                 <h3 class="f25 fw600 marT20">Registrarme</h3>
