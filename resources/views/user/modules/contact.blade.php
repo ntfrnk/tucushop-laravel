14663c0a44ba9d37a7988cd612042a983f9f3d0f
Franco D. Ocaranza <netfrank777@gmail.com>
2020-05-23 23:22:56 -0300

    Corrección en contacto

--- resources/views/user/modules/contact.blade.php
+++ resources/views/user/modules/contact.blade.php
@@ -25,7 +25,7 @@
                         <label for="phone" class="col-md-3 col-form-label">{{ __('Celular') }}</label>
 
                         <div class="col-md-5">
-                            <input id="phone" type="number" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ $user->addresses->first()->phone ? $user->addresses->first()->phone : '' }}" autocomplete="off">
+                            <input id="phone" type="number" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ $user->addresses->first()->phone ?? $user->addresses->first()->phone }}" autocomplete="off">
 
                             @error('phone')
                                 <span class="invalid-feedback" role="alert">
@@ -39,7 +39,7 @@
                         <label for="address" class="col-md-3 col-form-label">{{ __('Dirección') }}</label>
 
                         <div class="col-md-5 input-group">
-                            <input type="text" id="address" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ $user->addresses->first()->address ? $user->addresses->first()->address : '' }}" autocomplete="off">
+                            <input type="text" id="address" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ $user->addresses->first()->address ?? $user->addresses->first()->address }}" autocomplete="off">
 
                             @error('address')
                                 <span class="invalid-feedback" role="alert">
@@ -53,7 +53,7 @@
                         <label for="city" class="col-md-3 col-form-label">{{ __('Localidad') }}</label>
 
                         <div class="col-md-5 input-group">
-                            <input type="text" id="city" class="form-control @error('city') is-invalid @enderror" name="city" value="{{ $user->addresses->first()->city ? $user->addresses->first()->city : '' }}" autocomplete="off">
+                            <input type="text" id="city" class="form-control @error('city') is-invalid @enderror" name="city" value="{{ $user->addresses->first()->city ?? $user->addresses->first()->city }}" autocomplete="off">
 
                             @error('city')
                                 <span class="invalid-feedback" role="alert">
@@ -67,7 +67,7 @@
                         <label for="cp" class="col-md-3 col-form-label">{{ __('Código postal') }}</label>
 
                         <div class="col-md-5 input-group">
-                            <input type="text" id="postalcode" class="form-control @error('postalcode') is-invalid @enderror" name="postalcode" value="{{ $user->addresses->first()->postalcode ? $user->addresses->first()->postalcode : '' }}" autocomplete="off">
+                            <input type="text" id="postalcode" class="form-control @error('postalcode') is-invalid @enderror" name="postalcode" value="{{ $user->addresses->first()->postalcode ?? $user->addresses->first()->postalcode }}" autocomplete="off">
 
                             @error('postalcode')
                                 <span class="invalid-feedback" role="alert">
